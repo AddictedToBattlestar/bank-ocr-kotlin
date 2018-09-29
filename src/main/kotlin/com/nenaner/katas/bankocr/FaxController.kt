62337f85b69fd63ac934a122f0a1252c807310b6
John Ryan <addictedtobsg@gmail.com>
2018-09-28 21:39:30 -0400

    Moving forward with reading from the FaxReader.

--- src/main/kotlin/com/nenaner/katas/bankocr/FaxController.kt
+++ src/main/kotlin/com/nenaner/katas/bankocr/FaxController.kt
@@ -2,18 +2,38 @@ package com.nenaner.katas.bankocr
 
 import mu.KLogging
 
-class FaxController(faxReader: FaxReaderInterface) : FaxControllerInterface {
-    private val faxLine = arrayOf(
-            faxReader.readNextLine(),
-            faxReader.readNextLine(),
-            faxReader.readNextLine(),
-            faxReader.readNextLine())
+class FaxController(private val faxReader: FaxReaderInterface) : FaxControllerInterface {
+    private var faxLine: List<String> = ArrayList()
+
+    private var currentPosition = 0
 
     override fun readNextCharacter(): Array<String>? {
         logger.info { "FaxController.readNextCharacter called" }
-        return arrayOf(faxLine[0].substring(0, 3),
-                faxLine[1].substring(0, 3),
-                faxLine[2].substring(0, 3))
+        readNextFaxLineIfNecessary()
+        return getNextCharacterFromFaxLine()
+    }
+
+    private fun readNextFaxLineIfNecessary() {
+        if (faxLine.isEmpty() || currentPosition >= faxLine.get(0).length) {
+            currentPosition = 0
+            readNextFaxLine()
+        }
+    }
+
+    private fun readNextFaxLine() {
+        faxLine = listOf(
+                faxReader.readNextLine(),
+                faxReader.readNextLine(),
+                faxReader.readNextLine(),
+                faxReader.readNextLine())
+    }
+
+    private fun getNextCharacterFromFaxLine(): Array<String> {
+        val nextCharacter = arrayOf(faxLine[0].substring(currentPosition, currentPosition + 3),
+                faxLine[1].substring(currentPosition, currentPosition + 3),
+                faxLine[2].substring(currentPosition, currentPosition + 3))
+        currentPosition += 3
+        return nextCharacter
     }
 
     companion object : KLogging()
--- src/test/kotlin/com/nenaner/katas/bankocr/FaxControllerTests.kt
+++ src/test/kotlin/com/nenaner/katas/bankocr/FaxControllerTests.kt
@@ -1,8 +1,6 @@
 package com.nenaner.katas.bankocr
 
 import com.nhaarman.mockito_kotlin.mock
-import com.nhaarman.mockito_kotlin.times
-import com.nhaarman.mockito_kotlin.verify
 import com.nhaarman.mockito_kotlin.whenever
 import io.kotlintest.shouldBe
 import org.jetbrains.spek.api.Spek
@@ -14,20 +12,42 @@ val fakeDataLines =
         arrayOf("111222333444555666777888999",
                 "111222333444555666777888999",
                 "111222333444555666777888999",
-                "XXXXXXXXXXXXXXXXXXXXXXXXXXX")
+                "XXXXXXXXXXXXXXXXXXXXXXXXXXX",
+                "aaabbbcccdddeeefffggghhhiii",
+                "aaabbbcccdddeeefffggghhhiii",
+                "aaabbbcccdddeeefffggghhhiii"
+        )
 
 object FaxControllerTests : Spek({
     given("a Fax Reader") {
         val mockFaxReader = mock<FaxReaderInterface>()
-        whenever(mockFaxReader.readNextLine()).thenReturn(fakeDataLines[0]).thenReturn(fakeDataLines[1]).thenReturn(fakeDataLines[2]).thenReturn(fakeDataLines[3])
+        whenever(mockFaxReader.readNextLine())
+                .thenReturn(fakeDataLines[0])
+                .thenReturn(fakeDataLines[1])
+                .thenReturn(fakeDataLines[2])
+                .thenReturn(fakeDataLines[3])
+                .thenReturn(fakeDataLines[4])
+                .thenReturn(fakeDataLines[5])
+                .thenReturn(fakeDataLines[6])
         val faxController = FaxController(mockFaxReader)
-        on("reading the next character") {
+        on("trying to read the first character") {
             val returnValue = faxController.readNextCharacter()
-            it("makes a call to the Fax reader to read 4 lines") {
-                verify(mockFaxReader, times(4)).readNextLine()
-            }
             it("returns the first character image") {
-                returnValue shouldBe arrayOf("111","111","111")
+                returnValue shouldBe arrayOf("111", "111", "111")
+            }
+
+        }
+        on("trying to read the second character") {
+            val returnValue = faxController.readNextCharacter()
+            it("returns the second character image") {
+                returnValue shouldBe arrayOf("222", "222", "222")
+            }
+        }
+        on("trying to read the tenth character (on the next line") {
+            for (i in 3..9) faxController.readNextCharacter()
+            val returnValue = faxController.readNextCharacter()
+            it("returns the tenth character image") {
+                returnValue shouldBe arrayOf("aaa", "aaa", "aaa")
             }
         }
     }
